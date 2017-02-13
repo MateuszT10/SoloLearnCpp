a6edc5dc606cc96b377743c862a826cb2814be3b
Mateusz-Laptop\Mateusz <t.mateusz10@gmail.com>
2017-02-13 18:13:53 +0100

    Pierwszy commit

--- SoloLearnCpp/SoloLearnCpp.cpp
+++ SoloLearnCpp/SoloLearnCpp.cpp
@@ -1,11 +1,9 @@
-// SoloLearnCpp.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    return 0;
+	cout << "Hello world!";
+	return 0;
 }
-
